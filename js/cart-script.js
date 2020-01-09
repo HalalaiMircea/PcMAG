c474368e657d344b0ab4296d2afe212eaf6fbbfe
Halalai Mircea <boss.mircea67@gmail.com>
2020-01-09 23:15:04 +0200

    Now items can be removed from the cart

--- js/cart-script.js
+++ js/cart-script.js
@@ -1,26 +1,60 @@
-//<link rel="stylesheet" type="text/css" href="css/empty-cart-style.css">
-/*<h1>Your cart is empty 😢</h1>
-<h4>Please return to the store to add products</h4>
-<a href="index.html">
-    <div id="return-bttn-cont">Return to Store</div>
-</a>*/
-
+const root = 'https://jsonplaceholder.typicode.com/photos/';
+let products_json;
+let item_array;
+let cart_content_div;
 
 // This function displays the cart HTML page depending on what content there is in localStorage
 function displayContent() {
-    if (localStorage.getItem('item_array') == null) {
-        let css_style = document.createElement('link');
-        css_style.rel = 'stylesheet';
-        css_style.type = 'text/css';
+    let head_tag = document.getElementsByTagName('head').item(0);
+    let css_style = document.createElement('link');
+    css_style.rel = 'stylesheet';
+    css_style.type = 'text/css';
+    item_array = JSON.parse(localStorage.getItem('item_array'));
+    cart_content_div = document.getElementById('cart-content-div');
+
+    if (item_array.length === 0) {
         css_style.href = 'css/empty-cart-style.css';
-        document.getElementsByTagName('head').item(0).appendChild(css_style);
-        document.getElementById('empty-cart-div').innerHTML = "<h1>Your cart is empty 😢</h1>\n" +
-            "<h4>Please return to the store to add products</h4>\n" +
-            "<a href=\"index.html\">\n" +
-            "    <div id=\"return-bttn-cont\">Return to Store</div>\n" +
-            "</a>"
+        head_tag.appendChild(css_style);
+        setTimeout(displayEmptyCart, 100);
     } else {
         console.log('Cart is not empty');
-        
+        css_style.href = 'css/cart-style.css';
+        head_tag.appendChild(css_style);
+        setTimeout(displayFullCart, 100);
     }
+}
+
+function displayEmptyCart() {
+    cart_content_div.innerHTML = "<h1>Your cart is empty 😢</h1>\n" +
+        "<h4>Please return to the store to add products</h4>\n" +
+        "<a href=\"index.html\">\n" +
+        "    <div id=\"return-bttn-cont\">Return to Store</div>\n" +
+        "</a>"
+}
+
+function displayFullCart() {
+    let h1_tag = document.createElement('h1');
+    h1_tag.textContent = 'My Cart';
+    h1_tag.id = 'cart-h1';
+    document.getElementById('main-body').insertBefore(h1_tag, cart_content_div);
+    for (let i = 0; i < item_array.length; i++) {
+        let a_elem = document.createElement('a');
+        a_elem.className = 'item-link';
+        a_elem.href = '#';
+        a_elem.innerHTML = '<img class="item-img" src="' + item_array[i].url + '" alt="">';
+        a_elem.onclick = function () {
+            removeYourSelf(i, a_elem);
+        };
+        // a_elem.addEventListener('click', removeYourSelf(i, a_elem));
+        cart_content_div.appendChild(a_elem);
+    }
+}
+
+function removeYourSelf(array_index, html_element) {
+    // Remove the item from the array at that index
+    item_array.splice(array_index, 1);
+    // Update the localstorage
+    localStorage.setItem('item_array', JSON.stringify(item_array));
+    // And remove the Node from the HTML doc
+    cart_content_div.removeChild(html_element);
 }
--- js/homepage-script.js
+++ js/homepage-script.js
@@ -2,21 +2,16 @@ const root = 'https://jsonplaceholder.typicode.com/photos/';
 let id = 1;
 const f_items_div = document.getElementsByClassName('featured-items-div').item(0);
 let products_json;
+let item_array;
 
 function createLinkElem(img_id, src_url) {
     let a_elem = document.createElement('a');
     a_elem.className = 'item-link';
     a_elem.href = '#';
     a_elem.innerHTML = '<img class="item-img" src="' + src_url + '" alt="">';
-    // Function for each link in the homepage to add it's specific item data to localstorage
+    // Function for each link in the homepage to add its specific item data to localstorage
     a_elem.onclick = function () {
-        let item_array;
-        if (localStorage.getItem('item_array') == null) {
-            item_array = [];
-            //localStorage.setItem('item_array', JSON.stringify(item_array));
-        } else {
-            item_array = JSON.parse(localStorage.getItem('item_array'));
-        }
+
         // We save it's ID from the DB and it's URL
         item_array.push({img_id: img_id, url: src_url});
         localStorage.setItem('item_array', JSON.stringify(item_array));
@@ -47,4 +42,14 @@ function getPost() {
         .catch(err => {
             console.log('ERROR: ', err.message);
         });
+}
+
+// On first load we want to set an empty array in localstorage so we won't have problems later
+function onFirstLoad() {
+    if (localStorage.getItem('item_array') == null) {
+        item_array = [];
+        localStorage.setItem('item_array', JSON.stringify(item_array));
+    } else
+        item_array = JSON.parse(localStorage.getItem('item_array'));
+    getPost();
 }
